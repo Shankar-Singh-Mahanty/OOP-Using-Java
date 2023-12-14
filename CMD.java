e4ac73b90befaa715aaf04bcdddcbe568d8ec904
Shankar Singh Mahanty <107379661+Shankar-Singh-Mahanty@users.noreply.github.com>
2023-12-14 13:44:23 +0530

    Add files via upload

---
+++ CMD.java
@@ -0,0 +1,5 @@
+class CMD {
+    public static void main(String[] args) {
+        System.out.println("My university name is: " +args[0]);
+    }
+}
---
+++ CMD2.java
@@ -0,0 +1,7 @@
+public class CMD2 {
+    public static void main(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
